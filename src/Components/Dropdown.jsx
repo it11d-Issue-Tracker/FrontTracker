a68b106c5887f6df6a74231fda593ec9a3a1972c
Samuel Cille <samuel.cilleruelo@estudiantat.upc.edu>
2025-05-23 23:58:04 +0200

    component dropdown preparat

---
+++ src/Components/Dropdown.jsx
@@ -0,0 +1,19 @@
+import React, {use, useState} from 'react'
+
+export default function Dropdown({ options, value, onChange, placeholder }) {
+  
+  return (
+    <select 
+      value={value} 
+      onChange={onChange} 
+      className="Dropdown"
+    >
+    <option value="">{placeholder || "Selecciona una opción"}</option>
+      {/*{options.map((option) => (
+        <option key={option.id} value={option.id}>
+          {option.name}
+        </option>
+      ))}*/}
+    </select>
+  )
+}
