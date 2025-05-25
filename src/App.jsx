d962edc20ca67cb871d3c9509047d874074619ba
Samuel Cille <samuel.cilleruelo@estudiantat.upc.edu>
2025-05-25 20:08:18 +0200

    logica user afegida

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Dropdown from './Components/Dropdown'
 import VistaEscogirUsuari from './vistes/VistaEscogirUsuari'
+import { AuthProvider } from './AuthContext';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <VistaEscogirUsuari/>
+      <AuthProvider>
+        <VistaEscogirUsuari/>
+      </AuthProvider>
     </div>
   )
 }
---
+++ src/data/users.js
@@ -0,0 +1,6 @@
+
+export const users = [
+    { id: 1, name: "Marti", apiKey: "33" },
+    { id: 2, name: "mar", apiKey: "34" },
+    { id: 3, name: "Samuel", apiKey: "vini" },
+  ];
