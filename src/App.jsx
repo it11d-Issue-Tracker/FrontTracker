d200f5e4a62b5cf95adcf07a96e597653c58cbbb
ShenceZhou <shence.zhou@estudiantat.upc.edu>
2025-05-30 04:40:11 +0200

    feature: Añadida e implementada la VistaCreadoraIssue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, useParams, Link } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useParams,
+    Link,
+} from "react-router-dom";
 import VistaEscogirUsuari from "./Components/VistaEscogirUsuari.jsx";
 import VistaPerfil from "./Components/VistaPerfil.jsx";
 import VistaSettings from "./Components/VistaSettings.jsx";
+import VistaCreadoraIssue from "./Components/VistaCreadoraIssue.jsx";
 import { AuthProvider } from "./AuthContext";
 import "./App.css";
 
@@ -10,15 +17,23 @@ const VistaPerfilWrapper = () => {
     return <VistaPerfil idUsuari={id} />;
 };
 
+const VistaCreadoraIssueWrapper = () => {
+    const { id } = useParams();
+    return <VistaCreadoraIssue userId={id} />;
+};
+
 function App() {
     return (
         <Router>
             <AuthProvider>
-                <Routes>
-                    <Route path="/" element={<VistaEscogirUsuari />} />
-                    <Route path="/perfil/:id" element={<VistaPerfilWrapper />} />
-                    <Route path="/settings" element={<VistaSettings />} />
-                </Routes>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<VistaEscogirUsuari />} />
+                        <Route path="/perfil/:id" element={<VistaPerfilWrapper />} />
+                        <Route path="/settings" element={<VistaSettings />} />
+                        <Route path="/crear-issue/:id" element={<VistaCreadoraIssueWrapper />} />
+                    </Routes>
+                </div>
             </AuthProvider>
         </Router>
     );
@@ -26,10 +41,14 @@ function App() {
 
 export default App;
 
-//                    <Link to="/" style={{ marginRight: 15 }}>
-//                         Seleccionar Usuari
-//                     </Link>
-
-//                <nav style={{ padding: "10px", background: "#f7f9fc" }}>
-//                     <Link to="/settings">Settings</Link>
-//                 </nav>
+//                    <nav style={{ padding: "10px", background: "#f7f9fc" }}>
+//                         <Link to="/" style={{ marginRight: 15 }}>
+//                             Seleccionar Usuari
+//                         </Link>
+//                         <Link to="/settings" style={{ marginRight: 15 }}>
+//                             Settings
+//                         </Link>
+//                         <Link to="/crear-issue/1">
+//                             Crear Issue (Usuari 1)
+//                         </Link>
+//                     </nav>
